5897353afcb4c4b40de5e4520aa796ea33680687
Harsh Vardhan Singh 202082 <87150873+Harsh-Vardhan-Singh-202082@users.noreply.github.com>
2021-09-24 15:54:53 +0530

    Add files via upload

--- Untitled-1.c
+++ Untitled-1.c
@@ -2,23 +2,17 @@
 
 int main(){
   int rows, columns, Element, count=0;
-
- 
   printf("Enter the number of Rows: \n");
   scanf("%d", &rows);
  printf("Enter the number of Columns: \n");
   scanf("%d", &columns);
-  int array[rows][columns];
-
-  
+  int array[rows][columns]; 
   printf("Enter %d elements: \n", (rows*columns));
   for(int i=0; i<rows; i++){
     for(int j=0; j<columns; j++){
       scanf("%d", &array[i][j]);
     }
-  }
-
-  
+  } 
   printf("Enter the element to get the position: \n");
   scanf("%d", &Element);
 
@@ -31,10 +25,7 @@ int main(){
       }
     }
   }
-
- 
   if(count==0)
     printf("No such Element exists \n");
-
   return 0;
 }
