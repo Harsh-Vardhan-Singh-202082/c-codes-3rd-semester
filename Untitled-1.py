334b7fb5bf3f45add647cd03190e52942a0fafc8
Harsh Vardhan Singh 202082 <87150873+Harsh-Vardhan-Singh-202082@users.noreply.github.com>
2022-11-11 11:52:58 +0530

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,17 +1,39 @@
-def printJobScheduling(arr, t):
-    n = len(arr)
-    for i in range(n):
-        for j in range(n - 1 - i):
-            if arr[j][2] < arr[j + 1][2]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-  
-    result = [False] * t
-    job = ['-1'] * t
-    for i in range(len(arr)):
-        for j in range(min(t - 1, arr[i][1] - 1), -1, -1):
-            if result[j] is False:
-                result[j] = True
-                job[j] = arr[i][0]
-                break
-  
-    print(job)
+
+class Graph(object):
+    def __init__(self, size):
+        self.adjMatrix = []
+        for i in range(size):
+            self.adjMatrix.append([0 for i in range(size)])
+        self.size = size
+    def add_edge(self, v1, v2):
+        if v1 == v2:
+            print("Same vertex %d and %d" % (v1, v2))
+        self.adjMatrix[v1][v2] = 1
+        self.adjMatrix[v2][v1] = 1
+    def remove_edge(self, v1, v2):
+        if self.adjMatrix[v1][v2] == 0:
+            print("No edge between %d and %d" % (v1, v2))
+            return
+        self.adjMatrix[v1][v2] = 0
+        self.adjMatrix[v2][v1] = 0
+
+    def __len__(self):
+        return self.size
+    def print_matrix(self):
+        for row in self.adjMatrix:
+            for val in row:
+                print('{:4}'.format(val)),
+            print
+def main():
+    g = Graph(5)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(1, 2)
+    g.add_edge(2, 0)
+    g.add_edge(2, 3)
+
+    g.print_matrix()
+
+
+if __name__ == '__main__':
+    main()
