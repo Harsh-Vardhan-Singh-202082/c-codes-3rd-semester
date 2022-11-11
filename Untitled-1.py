07d3f80ad858a2e8d258601ea12c6dc41395c752
Harsh Vardhan Singh 202082 <87150873+Harsh-Vardhan-Singh-202082@users.noreply.github.com>
2022-11-11 12:03:11 +0530

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,30 +1,13 @@
 
-def shortestPath(graph, u, v, k):
-	V = 4
-	INF = 999999999999
-	
-	# Base cases
-	if k == 0 and u == v:
-		return 0
-	if k == 1 and graph[u][v] != INF:
-		return graph[u][v]
-	if k <= 0:
-		return INF
-	res = INF
-	for i in range(V):
-		if graph[u][i] != INF and u != i and v != i:
-			rec_res = shortestPath(graph, i, v, k - 1)
-			if rec_res != INF:
-				res = min(res, graph[u][i] + rec_res)
-	return res
-if __name__ == '__main__':
-	INF = 999999999999
-	graph = [[0, 10, 3, 2],
-			[INF, 0, INF, 7],
-			[INF, INF, 0, 6],
-			[INF, INF, INF, 0]]
-	u = 0
-	v = 3
-	k = 2
-	print("Weight of the shortest path is",
-			shortestPath(graph, u, v, k))
+lis = [1, 2, 1, 2, 3, 4, 5, 1, 1, 2, 5, 6, 7, 8, 9, 9]
+
+uniqueList = []
+duplicateList = []
+
+for i in lis:
+	if i not in uniqueList:
+		uniqueList.append(i)
+	elif i not in duplicateList:
+		duplicateList.append(i)
+
+print(duplicateList)
